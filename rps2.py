d76c45cd3c903aad4102d8eeb7521475bd2d126d
Krystal <lopezkrystal10@gmail.com>
2022-12-07 16:31:00 -0800

    updated variable names

--- rps2.py
+++ rps2.py
@@ -36,23 +36,23 @@ print(scissors)
 
 choices = [rock, paper, scissors]
 
-user_pick = int(
+player_pick = int(
     input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(choices[user_pick])
+print(choices[player_pick])
 
-computer_pick = random.randint(0, 2)
+comp_pick = random.randint(0, 2)
 print("Computer pick was: ")
-print(choices[computer_pick])
+print(choices[comp_pick])
 
-if user_pick >= 3 or user_pick < 0:
+if player_pick >= 3 or player_pick < 0:
     print("That is an invalid choice. Please choose a number between 0 and 3")
-elif user_pick == 0 and computer_pick == 2:
+elif player_pick == 0 and comp_pick == 2:
     print("Rock smashes scissors, you win!")
-elif computer_pick == 0 and user_pick == 2:
+elif comp_pick == 0 and player_pick == 2:
     print("Rock smashes scissors, you lose!")
-elif computer_pick > user_pick:
+elif comp_pick > player_pick:
     print("You lose, the computer beat you!")
-elif user_pick > computer_pick:
+elif player_pick > comp_pick:
     print("You win, the computer is a loser!")
-elif computer_pick == user_pick:
+elif comp_pick == player_pick:
     print("It's a tie!")
